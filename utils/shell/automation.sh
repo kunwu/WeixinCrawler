86d07f4775bd64e8672ead8d986e619c23f9571c
WU Kun <kent.woo@gmail.com>
2014-10-10 11:57:43 +0800

    add stop control for automation.sh

--- utils/shell/automation.sh
+++ utils/shell/automation.sh
@@ -5,5 +5,11 @@ do
     date
     echo Start WeixinCrawler ...
     mvn compile exec:java -Dexec.mainClass="com.weiboyi.utils.weixin.WeixinCrawler.App" -Dexec.args="_xpath searchOfficialsOnly"
+    echo Check automation flag file existence.
+    if [ ! -f ./automation.flag ]; then
+        echo File ./automation.flag not existed. Quit.
+        exit 0
+    fi
+    echo Sleep 10 seconds then repeat.
     sleep 10
 done
